15591742b122b2f59212c65a6ab46a0fa4d025b4
d1gits <kasper@thedutchweb.nl>
2017-07-28 12:58:22 +0200

    Switch up the contracts in the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import SciFiContract from '../build/contracts/SciFi.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -44,17 +44,15 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const SciFi = contract(SciFiContract)
+    SciFi.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var SciFiInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
+      SciFi.deployed().then((instance) => {
         // Stores a given value, 5 by default.
         return simpleStorageInstance.set(5, {from: accounts[0]})
       }).then((result) => {
