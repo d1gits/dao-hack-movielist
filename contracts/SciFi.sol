6f4a8e02f449217210b4016d305ad7d274572b7b
Kasper Meilgaard <kasper@borges.xyz>
2017-07-28 12:56:40 +0200

    make vote payable and add pragma 0.4.2 for now

--- contracts/SciFi.sol
+++ contracts/SciFi.sol
@@ -1,8 +1,10 @@
+pragma solidity ^0.4.2;
+
 contract SciFi {
     mapping(bytes32 => uint) public bids;
     bytes32[1000000] public movies;
     uint public movie_num;
-    function vote(bytes32 name) {
+    function vote(bytes32 name) payable {
         if (msg.value==0)
             return;
         uint val=bids[name];
