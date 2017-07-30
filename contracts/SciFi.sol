8169fade15bce532ebc654686ec65c1e1b94c5b3
Kasper Meilgaard <kasper@borges.xyz>
2017-07-30 16:14:13 +0200

    Start adjusting SciFi to add a dao attack vulnerability

--- contracts/SciFi.sol
+++ contracts/SciFi.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.4.2;
 
 contract SciFi {
     mapping(bytes32 => uint) public bids;
+    mapping (address => uint) private userBalances;
     bytes32[1000000] public movies;
     uint public movie_num;
+
     function vote(bytes32 name) payable {
         if (msg.value==0)
             return;
@@ -12,5 +14,11 @@ contract SciFi {
             movies[movie_num++]=name;
         }
         bids[name]+=msg.value;
+    }
+
+    function withdrawBalance() public {
+        uint amountToWithdraw = userBalances[msg.sender];
+        userBalances[msg.sender] = 0;
+        if (!(msg.sender.call.value(amountToWithdraw)())) { throw; }
     }
 }
