76c8ab7e8a3989f9faaae2b01a3157b023403c05
d1gits <kasper@thedutchweb.nl>
2017-07-30 20:39:41 +0200

    Remove deploy dumbDAO

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 var SciFi = artifacts.require("./SciFi.sol");
-var dumbDAO = artifacts.require("./dumbDAO.sol");
 var Attacker = artifacts.require("./Attacker.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SciFi);
-  deployer.deploy(dumbDAO);
   deployer.deploy(Attacker);
 };
