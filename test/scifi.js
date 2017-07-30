4033c2984b154885c363bbdb2fcf8fe6d51ba07f
d1gits <kasper@thedutchweb.nl>
2017-07-30 16:14:13 +0200

    Start adjusting SciFi to add a dao attack vulnerability

---
+++ test/scifi.js
@@ -0,0 +1,19 @@
+var SciFi = artifacts.require("./SciFi.sol");
+//import getWeb3 from './utils/getWeb3'
+
+contract('SciFi', function(accounts) {
+
+  // it("...should store the value 89.", function() {
+  //   return SciFi.deployed().then(function(instance) {
+  //     sciFiInstance = instance;
+  //     const hexMovieName = 'Return of the Jedi'
+  //     return SciFiInstance.vote(hexMovieName, {from: accounts[0], value: 10000)
+  //     // return sciFiInstance.vote(89, {from: accounts[0]});
+  //   }).then(function() {
+  //     // return sciFiInstance.get.call();
+  //   }).then(function(storedData) {
+  //     // assert.equal(storedData, 89, "The value 89 was not stored.");
+  //   });
+  // });
+
+});
