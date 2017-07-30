946a879302b2a3fbed0186984af854b08dfe2106
Kasper Meilgaard <kasper@borges.xyz>
2017-07-30 16:13:32 +0200

    Use joeb000 mock-dao-attack repo's contracts to remake the dao attack

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var SciFi = artifacts.require("./SciFi.sol");
+var dumbDAO = artifacts.require("./dumbDAO.sol");
+var Attacker = artifacts.require("./Attacker.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SciFi);
+  deployer.deploy(dumbDAO);
+  deployer.deploy(Attacker);
 };
