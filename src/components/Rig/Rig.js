bba461f251403ce5acf5e21d982dc50be9ad6e27
d1gits <kasper@thedutchweb.nl>
2017-08-04 14:56:16 +0200

    Move event handling to the main component and cleanup

--- src/components/Rig/Rig.js
+++ src/components/Rig/Rig.js
@@ -2,27 +2,15 @@ import React, { Component } from 'react'
 
 class RigComponent extends Component {
 
-  // submits the rigging of the game
-  handleSubmit(event) {
-    const {rigTheGame} = this.props;
-    event.preventDefault();
-    rigTheGame()
-  }
-
   render() {
-    const {movieData, handleChange} = this.props,
-          {handleSubmit}            = this;
-          
+    const {handleChange, rigTheGame} = this.props;
+
     return (
 			<div className="content-box">
 				<div className="content-box--title" >Rig the game</div>
 				<p className="content-box--explanation">You can also just rig the game by using this contract. It allows you to steal whatever is in the contract and then you can just re-allocate that money to your favorite movie!</p>
-					<form onSubmit={handleSubmit} name="rig">
-						<div className="form-group">
-							<label>Movie name</label>
-							<input type="text" value={movieData.riggedName} onChange={handleChange} className="form-control" name="riggedName" placeholder="Name"></input>
-						</div>
-						<button type="submit" className="btn btn-default">Rig it!</button>
+					<form onSubmit={rigTheGame} name="rig">
+						<button type="submit" className="btn btn-default">Steal eth!</button>
 					</form>
 			</div>
     );
