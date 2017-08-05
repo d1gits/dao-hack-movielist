e79417d4af9c6176c02dea5506a25a1358247251
Kasper Meilgaard <kasper@borges.xyz>
2017-08-05 11:15:21 +0200

    untabify..

--- src/components/Rig/Rig.js
+++ src/components/Rig/Rig.js
@@ -6,13 +6,13 @@ class RigComponent extends Component {
     const {handleChange, rigTheGame} = this.props;
 
     return (
-			<div className="content-box">
-				<div className="content-box--title" >Rig the game</div>
-				<p className="content-box--explanation">You can also just rig the game by using this contract. It allows you to steal whatever is in the contract and then you can just re-allocate that money to your favorite movie!</p>
-					<form onSubmit={rigTheGame} name="rig">
-						<button type="submit" className="btn btn-default">Steal eth!</button>
-					</form>
-			</div>
+      <div className="content-box">
+        <div className="content-box--title" >Rig the game</div>
+        <p className="content-box--explanation">You can also just rig the game by using this contract. It allows you to steal whatever is in the contract and then you can just re-allocate that money to your favorite movie!</p>
+          <form onSubmit={rigTheGame} name="rig">
+            <button type="submit" className="btn btn-default">Steal eth!</button>
+          </form>
+      </div>
     );
   }
 }
--- test/attacker.js
+++ test/attacker.js
@@ -5,63 +5,63 @@ contract('Attacker', (accounts) => {
   describe('Steal some Moulah/Cheese/Ether', function() {
     it("...should steal all the ether from the SciFi contract.", () => {
 
-			// Declaring this for later so we can chain functions on Attacker and SciFi
-	    let AttackerInstance
-	    let SciFiInstance
+      // Declaring this for later so we can chain functions on Attacker and SciFi
+      let AttackerInstance
+      let SciFiInstance
 
-			// Set the initial balance
-			let initialBalance = web3.eth.getBalance(accounts[0])
+      // Set the initial balance
+      let initialBalance = web3.eth.getBalance(accounts[0])
       console.log(initialBalance)
 
-			const doAttack = () => {
-				return AttackerInstance.fundMe(
-					{from: accounts[0], gas:200000, value: web3.toWei(5,'ether')})
-					.then(() => {
-						return AttackerInstance.setSciFiAddress(
-							SciFiInstance.address,
-							{from: accounts[0], gas:200000})
-					})
-					.then(() => {
-	           return AttackerInstance.vote(
-							 web3.toHex('Coolmovie'),
-							 web3.toWei(5,'ether'),
-							 {from: accounts[0], gas:200000})
-					})
-					.then(() => {
-	          return AttackerInstance.stealEth({from: accounts[0], gas:400000})
-					})
-					.then(() => {
-						return AttackerInstance.payOut({from: accounts[0], gas:200000})
-					})
-			}
+      const doAttack = () => {
+        return AttackerInstance.fundMe(
+          {from: accounts[0], gas:200000, value: web3.toWei(5,'ether')})
+          .then(() => {
+            return AttackerInstance.setSciFiAddress(
+              SciFiInstance.address,
+              {from: accounts[0], gas:200000})
+          })
+          .then(() => {
+             return AttackerInstance.vote(
+               web3.toHex('Coolmovie'),
+               web3.toWei(5,'ether'),
+               {from: accounts[0], gas:200000})
+          })
+          .then(() => {
+            return AttackerInstance.stealEth({from: accounts[0], gas:400000})
+          })
+          .then(() => {
+            return AttackerInstance.payOut({from: accounts[0], gas:200000})
+          })
+      }
 
-	    // Get accounts.
+      // Get accounts.
       return SciFi.deployed().then((instance) => {
         SciFiInstance = instance
         return Attacker.deployed().then((instance) => {
           AttackerInstance = instance
-					return SciFiInstance.vote(
-						web3.toHex('XYZ'),
-						{from: accounts[1], gas:200000, value: web3.toWei(5,'ether')
-					})
-	        .then((result) => {
-						return doAttack()
-	        })
-					.then(() =>{
+          return SciFiInstance.vote(
+            web3.toHex('XYZ'),
+            {from: accounts[1], gas:200000, value: web3.toWei(5,'ether')
+          })
+          .then((result) => {
+            return doAttack()
+          })
+          .then(() =>{
             let currBalance = web3.eth.getBalance(accounts[0]);
             console.log(currBalance)
-						// this represents 0.6 Eth if we compare it to the first part of the
-						// bignumber (c[0]) which is the most reasonable comparison I could
-						// think of for now but there might be a better way :)
-						// also this is a lot of gas, so everything shoud be more efficient..
-						let maxGas = 6000
-						return assert.isAtMost(
-							initialBalance.c[0],
-							currBalance.c[0] + maxGas,
-							"Got ethereum back minus some transaction cost.");
+            // this represents 0.6 Eth if we compare it to the first part of the
+            // bignumber (c[0]) which is the most reasonable comparison I could
+            // think of for now but there might be a better way :)
+            // also this is a lot of gas, so everything shoud be more efficient..
+            let maxGas = 6000
+            return assert.isAtMost(
+              initialBalance.c[0],
+              currBalance.c[0] + maxGas,
+              "Got ethereum back minus some transaction cost.");
 
-					})
-				})
+          })
+        })
       })
     })
 
