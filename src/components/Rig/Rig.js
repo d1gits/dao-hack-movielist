0c954b0b7c6a7db6b9b1dfbe6a812843ef917bd0
d1gits <kasper@thedutchweb.nl>
2017-08-08 08:26:05 +0200

    Cleanup app and update rig components

--- src/components/Rig/Rig.js
+++ src/components/Rig/Rig.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 class RigComponent extends Component {
 
   render() {
-    const {handleChange, rigTheGame} = this.props;
+    const {rigTheGame} = this.props;
 
     return (
       <div className="content-box">
         <div className="content-box--title" >Rig the game</div>
-        <p className="content-box--explanation">You can also just rig the game by using this contract. It allows you to steal whatever is in the contract and then you can just re-allocate that money to your favorite movie!</p>
+        <p className="content-box--explanation">You can also just rig the game by using this contract. It allows you to steal up to 5 ether per time from the contract (per 1 whole ether) and then you can just re-allocate that money to your favorite movie!</p>
           <form onSubmit={rigTheGame} name="rig">
             <button type="submit" className="btn btn-default">Steal eth!</button>
           </form>
