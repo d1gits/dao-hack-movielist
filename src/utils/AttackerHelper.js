d788ecb279c383b564de5d9cbf0ccbca9afe04da
Kasper Meilgaard <kasper@borges.xyz>
2017-08-08 08:25:47 +0200

    Adjust the helpers for the new contracts

--- src/utils/AttackerHelper.js
+++ src/utils/AttackerHelper.js
@@ -11,7 +11,7 @@ class AttackerHelper {
    doAttack (movieName, web3, accounts, AttackerInstance, SciFiInstance) {
 
     return AttackerInstance.fundMe(
-      {from: accounts[0], gas:200000, value: web3.toWei(5,'ether')})
+      {from: accounts[0], gas:200000, value: web3.toWei(1,'ether')})
       .then(() => {
         return AttackerInstance.setSciFiAddress(
           SciFiInstance.address,
@@ -19,7 +19,7 @@ class AttackerHelper {
         .then(() => {
            return AttackerInstance.vote(
              web3.toHex(movieName),
-             web3.toWei(5,'ether'),
+             web3.toWei(1,'ether'),
              {from: accounts[0], gas:400000})
            .then(() => {
              return AttackerInstance.stealEth({from: accounts[0], gas:1000000})
@@ -32,18 +32,17 @@ class AttackerHelper {
   }
 
   // Attack function
-  // @TODO: break this beast up
   attackSciFi(web3, movieData) {
     return new Promise((resolve, reject) => {
-      const {attackerContract, sciFiContract} = this;
-      const movieName = movieData.riggedName
+      const {attackerContract, sciFiContract} = this,
+             movieName = movieData.riggedName;
 
       attackerContract.setProvider(web3.currentProvider)
       sciFiContract.setProvider(web3.currentProvider)
 
       // Declaring this for later so we can chain functions on Attacker.
-      var AttackerInstance
-      var SciFiInstance
+      let AttackerInstance
+      let SciFiInstance
       // Get accounts.
       web3.eth.getAccounts((error, accounts) => {
 
--- src/utils/SciFiHelper.js
+++ src/utils/SciFiHelper.js
@@ -4,10 +4,11 @@ class SciFiHelper {
     this.contract = SciFiContract;
 
     // bindings
-    this.getAllMovies    = this.getAllMovies.bind(this)
-    this.vote            = this.vote.bind(this)
-    this.withdrawVotes   = this.withdrawVotes.bind(this)
-    this.getSortedMovies = this.getSortedMovies.bind(this)
+    this.recursiveGetMovie = this.recursiveGetMovie.bind(this)
+    this.vote              = this.vote.bind(this)
+    this.withdrawVotes     = this.withdrawVotes.bind(this)
+    this.recursiveWithdraw = this.recursiveWithdraw.bind(this)
+    this.getSortedMovies   = this.getSortedMovies.bind(this)
 
   }
 
@@ -28,7 +29,6 @@ class SciFiHelper {
         return contract.deployed().then((instance) => {
           SciFiInstance = instance
           // vote for movie
-          console.log('vote for movie')
           return SciFiInstance.vote(
             hexMovieName,
             {from: accounts[0], gas:200000, value: web3.toWei(amount,'ether')
@@ -40,50 +40,97 @@ class SciFiHelper {
     })
   }
 
+  // returns a recursive promise building a list of all movies
+  recursiveWithdraw(movies, i, numVotes, SciFiInstance, accounts, web3) {
+    return SciFiInstance.votes(i).then((result)=>{
+
+      const owner    = result[0].toString(),
+            retracted = result[3];
+
+      return new Promise((resolve, reject) => {
+        if(!retracted && owner === accounts[0]) {
+          return SciFiInstance.withdrawVote(
+            i,
+            {from: accounts[0], gas:200000}).then(()=>{
+              resolve()
+            })
+        } else {
+          resolve()
+        }
+      }).then(()=>{
+        // get the next movie if we're not finished, otherwise: return the movies
+        if (i === numVotes) {
+          return;
+        } else {
+          return this.recursiveWithdraw(movies, i+1, numVotes, SciFiInstance, accounts, web3);
+        }
+      })
+
+
+    })
+  }
+
+
   withdrawVotes(web3) {
     return new Promise((resolve, reject) => {
       const {contract} = this;
-
       contract.setProvider(web3.currentProvider)
-
       // Declaring this for later so we can chain functions on contract.
-      var SciFiInstance
-
+      let SciFiInstance
       // Get accounts.
       return web3.eth.getAccounts((error, accounts) => {
-        return contract.deployed()
-        .then((instance) => {
+
+        return contract.deployed().then((instance) => {
           SciFiInstance = instance
-          // withdaws the money and resets the votes
-          return SciFiInstance.withdraw(
-            accounts[0],
-            {from: accounts[0], gas:500000})
-        })
-        .then((result) => {
-          resolve()
+
+          // Get the value from the contract to prove it worked.
+          return SciFiInstance.numVotes()
+
+          .then((result) => {
+
+            let numVotes = result.c[0];
+            let movies = [];
+            // console.log(numVotes)
+            return this.recursiveWithdraw(movies, 1, numVotes, SciFiInstance, accounts, web3)
+            .then(() => {
+              // resolve the initial promise with a sorted list of movies
+              resolve();
+            });
+          })
         })
       })
     })
   }
 
   // returns a recursive promise building a list of all movies
-  getAllMovies(movies, i, numMovies, SciFiInstance, web3) {
-    return SciFiInstance.movies(i).then((result)=>{
-      const hexname = result[0];
-      const amount = result[1].c[0];
+  recursiveGetMovie(movies, i, numVotes, SciFiInstance, web3) {
+    return SciFiInstance.votes(i).then((result)=>{
 
-      // if the amount is not 0 add the movie to the list
-      if (amount > 0){
-        movies = [...movies, {
-          name   : web3.toAscii(hexname),
-          amount : parseFloat(amount/10000)}
-        ]
+      const amount    = result[1].c[0],
+            hexname   = result[2],
+            retracted = result[3];
+
+      if(!retracted) {
+        // check if movie exists
+        if(movies.find((movie)=>{ return movie.name === web3.toAscii(hexname) })){
+          // adjust movie
+          const objIndex = movies.findIndex((movie)=>{ return movie.name === web3.toAscii(hexname)})
+          movies[objIndex].amount += amount/10000
+
+        } else {
+          // new movie
+          movies = [...movies, {
+            name   : web3.toAscii(hexname),
+            amount : parseFloat(amount/10000)}
+          ]
+        }
       }
+
       // get the next movie if we're not finished, otherwise: return the movies
-      if (i === numMovies) {
+      if (i === numVotes) {
         return movies;
       } else {
-        return this.getAllMovies(movies, i+1, numMovies, SciFiInstance, web3);
+        return this.recursiveGetMovie(movies, i+1, numVotes, SciFiInstance, web3);
       }
     })
   }
@@ -102,14 +149,14 @@ class SciFiHelper {
           SciFiInstance = instance
 
           // Get the value from the contract to prove it worked.
-          return SciFiInstance.numMovies()
+          return SciFiInstance.numVotes()
 
           .then((result) => {
 
-            let numMovies = result.c[0];
+            let numVotes = result.c[0];
             let movies = [];
-            // console.log(numMovies)
-            return this.getAllMovies(movies, 1, numMovies, SciFiInstance, web3)
+            // console.log(numVotes)
+            return this.recursiveGetMovie(movies, 1, numVotes, SciFiInstance, web3)
             .then((allMovies) => {
               // sort the movies
               let sortedMovies = allMovies.sort(
